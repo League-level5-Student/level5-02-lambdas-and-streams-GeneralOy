63b69594644f7d65d4adfb1cc4da7f0d0919b9ef
League Student <league@iMac-20.attlocal.net>
2020-01-16 16:57:43 -0800

    work on 02_streams - done with the previous ones - see links below
    http://tutorials.jenkov.com/java/lambda-expressions.html
    https://www.geeksforgeeks.org/lambda-expressions-java-8/
    https://stackify.com/streams-guide-java-8/

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -4,42 +4,49 @@ import java.awt.Desktop.Action;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
-public class LambdaButtons implements ActionListener {
+public class LambdaButtons {
+	private int no = 0;
+	private Random rando = new Random();
 	private JFrame window = new JFrame();
 	private JButton addNumbers = new JButton("ADD 2 Numbers");
 	private JButton randNumber = new JButton("RANDOM NUMBER");
 	private JButton tellAJoke = new JButton("TELL A JOKE");
-	
+
 	public LambdaButtons() {
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new FlowLayout());
 		window.add(addNumbers);
 		window.add(randNumber);
 		window.add(tellAJoke);
-		
-		//1. Call the addActionListener methods for each button. Use lambdas
-		//   to define to functionality of the buttons.
-		addNumbers.addActionListener(this);
-		randNumber.addActionListener(this);
-		tellAJoke.addActionListener(this);
+
+		// 1. Call the addActionListener methods for each button. Use lambdas
+		// to define to functionality of the buttons.
+		addNumbers.addActionListener((e) -> {
+			System.out.println("hi add");
+			no += 2;
+			JOptionPane.showMessageDialog(null, no);
+		});
+		randNumber.addActionListener((e) -> {
+			System.out.println("hi rand");
+			JOptionPane.showMessageDialog(null, rando.nextInt(1000));
+		});
+		tellAJoke.addActionListener((e) -> {
+			System.out.println("hi joke");
+			JOptionPane.showMessageDialog(null,
+					"Why did the Prophet of Truth call Allstate when the Parasite attacked? \n\nBecause he wanted to make sure he had Flood insurance :^)");
+		});
 		window.setVisible(true);
 		window.pack();
 	}
-	
+
 	public static void main(String[] args) {
 		new LambdaButtons();
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		
-		// TODO Auto-generated method stub
-		
-	}
-	
-	
 }
--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -1,29 +1,85 @@
 package _01_Lambda_Methods;
 
+import java.util.Random;
+
 public class LambdaMethods {
+	public static Random rando = new Random();
+
 	public static void main(String[] args) {
-		// 1. Look at the SpecialPrinter function interface.
-	
-		// Here is an example of calling the printCustomMessage method with a lambda.
-		// This prints the passed in String 10 times.
-		printCustomMessage((s)->{
-			for(int i = 0; i < 10; i++) {
+		/** 1. Look at the SpecialPrinter function interface. */
+
+		/*
+		 * Here is an example of calling the printCustomMessage method with a lambda. //
+		 * This prints the passed in String 10 times.
+		 */
+		printCustomMessage((s) -> {
+			for (int i = 0; i < 10; i++) {
 				System.out.println(s);
 			}
 		}, "repeat");
-		
-		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		
-		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
-		
-		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
-		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+
+		/**
+		 * 2. Call the printCustomMessage method using a lambda so that the String
+		 * prints backwards.
+		 */
+		printCustomMessage((s) -> {
+			String ret = "";
+			for (char c : s.toCharArray()) {
+				ret = c + ret;
+			}
+			System.out.println(ret);
+		}, "repeat");
+		/**
+		 * 3. Call the printCustomMessage method using a lambda so that the String
+		 * prints with a mix between upper an lower case characters.
+		 */
+		printCustomMessage((s) -> {
+			String ret = "";
+			for (char c : s.toCharArray()) {
+				if (rando.nextBoolean()) {
+					ret = ret + s.valueOf(c).toUpperCase();
+				} else {
+					ret = ret + s.valueOf(c).toLowerCase();
+				}
+			}
+			System.out.println(ret);
+		}, "repeat");
+		/**
+		 * 4. Call the printCustomMessage method using a lambda so that the String
+		 * prints with a period in between each character.
+		 */
+		printCustomMessage((s) -> {
+			String ret = "";
+			for (char c : s.toCharArray()) {
+				ret = ret + c + ".";
+			}
+			System.out.println(ret);
+		}, "repeat");
+		/**
+		 * 5. Call the printCustomMessage method using a lambda so that the String
+		 * prints without any vowels.
+		 */
+		printCustomMessage((s) -> {
+			String ret = "";
+			for (char c : s.toCharArray()) {
+				String vowels = "aAeEiIoOuU";
+				boolean isVow = false;
+				for (char v : vowels.toCharArray()) {
+					if (c == v) {
+						isVow = true;
+					}
+				}
+				if (!isVow) {
+					ret = ret + c;
+				}
+
+			}
+			System.out.println(ret);
+		}, "repeat");
+
 	}
-	
+
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
 		sp.printSpecial(value);
 	}
 }
-	
