0e7974a2494d6c323c848c1f93c9ebec8547e6f2
League Student <league@iMac-20.attlocal.net>
2020-01-23 16:07:55 -0800

    donezo
    :^)

--- src/_03_Sorting_With_Streams/StreamSorter.java
+++ src/_03_Sorting_With_Streams/StreamSorter.java
@@ -1,5 +1,6 @@
 package _03_Sorting_With_Streams;
 
+import java.awt.List;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Stream;
@@ -7,16 +8,18 @@ import java.util.stream.Stream;
 public class StreamSorter {
 	public static void main(String[] args) {
 		Integer[] nums = new Integer[50];
-		for(int i = 0; i < nums.length; i++) {
+		for (int i = 0; i < nums.length; i++) {
 			Random rGen = new Random();
 			nums[i] = rGen.nextInt();
 		}
-		
-		//1. Convert the Integer array to Stream object.
-		
-		//2. Use the sorted method of the stream to sort the numbers.
-		
-		//3. Use the forEach method with a lambda to print all the elements of the sorted Stream.
-		//   They should be in ascending order.
+		java.util.List<Integer> list = Arrays.asList(nums);
+
+		/** 1. Convert the Integer array to Stream object. */
+		list.stream().sorted().forEach(System.out::println);
+		/** 2. Use the sorted method of the stream to sort the numbers. */
+		/**
+		 * 3. Use the forEach method with a lambda to print all the elements of the
+		 * sorted Stream. // They should be in ascending order.
+		 */
 	}
 }
